b9b2555b17951756fb80db3ef8d1fc248c629e0e
lolieg <marvinlohmar123456789@gmail.com>
2021-03-15 18:45:49 +0100

    changed wrong socket io url

--- middleware/roomExists.js
+++ middleware/roomExists.js
@@ -1,7 +1,11 @@
 const io = require('socket.io-client')
 
 export default async function ({ params, redirect }) {
-  const socket = io('http://localhost:3000')
+  const socket = io(
+    process.env.NODE_ENV !== 'production'
+      ? 'http://localhost:3001'
+      : 'http://karaoke.speedy-schumachers.ml'
+  )
   const room = await new Promise((resolve) =>
     socket.emit('getRoom', { id: params.room }, (resp) => resolve(resp))
   )
